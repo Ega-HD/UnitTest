46d99bb868ec4101d1493b2386abc7a39331d31d
CrisDit <95460948+CrisDit@users.noreply.github.com>
2022-03-28 21:45:59 +0700

    Add files via upload

---
+++ Persegi.java
@@ -0,0 +1,11 @@
+
+public class Persegi {
+   
+    public double keliling(double sisi){
+        return sisi*4;
+    }
+    
+    public double luas(double sisi){
+        return sisi*sisi;
+    }
+}
