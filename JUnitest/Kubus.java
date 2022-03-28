467a9ef989c1a66d7d22b5e67ba42b3934ae733e
CrisDit <95460948+CrisDit@users.noreply.github.com>
2022-03-28 22:00:56 +0700

    Add files via upload

---
+++ JUnitest/Kubus.java
@@ -0,0 +1,10 @@
+public class Kubus extends Persegi {
+    
+    public double luasPermukaan(double rusuk){
+        return luas(rusuk)*6;
+    }
+
+    public double volume(double rusuk){
+        return luas(rusuk)*rusuk;
+    }
+}
